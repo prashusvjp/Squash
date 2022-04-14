49f5e576c3b2dc16352fe4dd5faaa933718221f9
Prashanth S <prashusvjp@gmail.com>
2022-04-14 22:57:46 +0530

    Initial commit

---
+++ app/src/main/java/com/example/squash/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.squash;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+
+
+    }
+}
